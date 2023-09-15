86689cc52f0229193ee715f768cb4713cdb7d6f1
Ignacio Lovato <igaciolovatop@gmail.com>
2023-09-15 15:51:54 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//importar componentes
+import { InicioComponent } from './inicio/inicio.component';
+import { AngularComponent } from './angular/angular.component';
+import { GitComponent } from './git/git.component';
+import { TerminalComponent } from './terminal/terminal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'angular', component:AngularComponent},
+  {path: 'git',component: GitComponent},
+  {path: 'terminal',component: TerminalComponent},
+  {path:"",redirectTo:"/inicio", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AngularComponent } from './angular/angular.component';
+import { TerminalComponent } from './terminal/terminal.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AngularComponent,
+    TerminalComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
